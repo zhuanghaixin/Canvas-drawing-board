1bd83c8b32fdb0a05dc5f84ae5e4e327060e85f5
zhuanghaixin <649308293@qq.xom>
2018-03-18 00:05:58 +0800

    modified:   canvas-drawing-board/css/style.css 	modified:   canvas-drawing-board/index.html 	modified:   canvas-drawing-board/js/index.js

--- canvas-drawing-board/js/index.js
+++ canvas-drawing-board/js/index.js
@@ -1,56 +1,94 @@
 var canvas = document.getElementById('canvas');
-//全屏
-var pageWidth=document.documentElement.clientWidth;
-var pageHeight=document.documentElement.clientHeight;
-canvas.width=pageWidth;
-canvas.height=pageHeight;
-
-window.onresize=function(){
-  var pageWidth=document.documentElement.clientWidth;
-  var pageHeight=document.documentElement.clientHeight;
-  canvas.width=pageWidth;
-  canvas.height=pageHeight;
-};
+var usingEraser = false;
 var context = canvas.getContext('2d');
-var painting=false;
-//设置终点
-var lastPoint={'x':undefined,'y':undefined};
-canvas.onmousedown = function(x) {
+autoSetCanvasSize(canvas);
+listenToMouse(canvas);
 
-  painting=true;
-  var offsetX = x.clientX;
-  var offsetY = x.clientY;
-  lastPoint={'x':offsetX,'y':offsetY};
-  console.log(lastPoint);
-  drawCircle(offsetX,offsetY);
-//   drawLine(offsetX,offsetY,offsetX+30,offsetY+30)
-};
 
-canvas.onmousemove = function(x) {
+//设置Canvas宽高
+function autoSetCanvasSize(canvas) {
+  setCanvasSize();
 
-  if(painting){
+  window.onresize = function() {
+    setCanvasSize();
+  };
+  //全屏
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth;
+    var pageHeight = document.documentElement.clientHeight;
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
+  }
+}
+
+//监听鼠标
+function listenToMouse(canvas) {
+
+
+
+  var using = false;
+  //设置终点
+  var lastPoint = {
+    'x': undefined,
+    'y': undefined
+  };
+  canvas.onmousedown = function(x) {
+
+    using = true;
     var offsetX = x.clientX;
     var offsetY = x.clientY;
-    var newPoint={'x':offsetX,'y':offsetY};
-//   drawCircle(offsetX,offsetY);
+
+    console.log(lastPoint);
+    if (usingEraser) {
+      context.clearRect(offsetX, offsetY+10, 10, 10);
+    } else {
+      lastPoint = {
+        'x': offsetX,
+        'y': offsetY
+      };
+
+    }
+    //   drawCircle(offsetX,offsetY);
+    //   drawLine(offsetX,offsetY,offsetX+30,offsetY+30)
+  };
+
+  canvas.onmousemove = function(x) {
 
 
-    drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
-    lastPoint=newPoint;
-  }
-};
-canvas.onmouseup = function(x) {
-  painting=false;
+    var offsetX = x.clientX;
+    var offsetY = x.clientY;
+    if (!using) {
+      return;
+    }
+    if (usingEraser) {
+      context.clearRect(offsetX, offsetY+10, 10, 10);
+    } else {
+      var newPoint = {
+        'x': offsetX,
+        'y': offsetY
 
-  var offsetX = x.clientX;
-  var offsetY = x.clientY;
-  drawCircle(offsetX,offsetY);
+      }
+      //   drawCircle(offsetX,offsetY);
 
-};
 
+      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+      lastPoint = newPoint;
+    }
+  };
+
+  canvas.onmouseup = function(x) {
+    using = false;
+
+    var offsetX = x.clientX;
+    var offsetY = x.clientY;
+    //   drawCircle(offsetX,offsetY);
+
+
+  };
+}
 
 //画圆
-function drawCircle(x,y) {
+function drawCircle(x, y) {
   context.fillStyle = 'blue';
   context.beginPath();
   context.arc(x, y, 1, 0, 2 * Math.PI);
@@ -58,12 +96,45 @@ function drawCircle(x,y) {
 }
 
 //画线
-function drawLine(x,y,x1,y1){
+function drawLine(x, y, x1, y1) {
+
   context.strokeStyle = 'blue';
-  context.lineWidth=1;
+  context.lineWidth = 10;
   context.beginPath();
-  context.moveTo(x,y);
-  context.lineTo(x1,y1);
+  context.moveTo(x, y +10);
+  context.lineTo(x1, y1 +10);
   context.closePath();
   context.stroke();
+
 }
+
+
+
+/*******************/
+//橡皮檫
+var eraser = document.getElementById('eraser');
+eraser.onclick = function() {
+  usingEraser = true;
+  console.log("正在使用橡皮擦" + usingEraser);
+  actions.className="actions x";
+  document.body.className = "p2";
+
+
+};
+//画笔
+var brush=document.getElementById('brush');
+brush.onclick = function() {
+  usingEraser = false;
+  console.log("正在使用画笔" + usingEraser);
+  actions.className="actions";
+  document.body.className = "p1";
+
+
+};
+//清屏
+var clearScreen = document.getElementById('clearScreen');
+clearScreen.onclick = function() {
+  var pageWidth = document.documentElement.clientWidth;
+  var pageHeight = document.documentElement.clientHeight;
+  context.clearRect(0, 0, pageWidth, pageHeight);
+};
